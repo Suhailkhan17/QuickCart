0bd65dea14681d259bddd48aa5a870d73adde6ec
Meenakshi <meenakshi745384@gmail.com>
2025-10-04 09:17:50 +0530

    First commit: QuickCart project setup

---
+++ config/db.js
@@ -0,0 +1,24 @@
+import mongoose, { MongooseError } from "mongoose";
+
+let cached = global.mongoose
+
+if (!cached){
+    cached= global.mongoose = { conn: null, promise: null }
+}
+async function connectDB(){
+    if(cached.conn){
+        return cached.conn
+    }
+    if(!cached.promise){
+        const opts = {
+            bufferCommands:false
+        }
+        cached.promise= (await mongoose.connect(`${process.env.MONGODB_URI}/quickcart`,opts)).then(mongoose =>{
+            return Mongoose
+        })
+    }
+    cached.com = await cached.promise
+    return cached.conn
+}
+
+export default connectDB
